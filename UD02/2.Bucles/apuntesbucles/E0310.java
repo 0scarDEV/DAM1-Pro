3078b970ede26d831de578a321f00b2c95a16870
0scarDEV <146084578+0scarDEV@users.noreply.github.com>
2023-10-18 14:26:45 +0200

    18Oct

---
+++ UD02/2.Bucles/apuntesbucles/E0310.java
@@ -0,0 +1,10 @@
+package apuntesbucles;
+
+public class E0310 {
+    // E0310. Diseñar un programa que muestre la suma de los 10 primeros números impares.
+    public static void main(String[] args) {
+        for (int i = 0; i     == 10; i++) {
+
+        }
+    }
+}
