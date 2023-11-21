2820ec1a3fc317640f174a5a42bce4b7da64d2d9
0scarDEV <146084578+0scarDEV@users.noreply.github.com>
2023-11-21 14:21:40 +0100

    21Nov

---
+++ UD03/1. Clases/clasesparaninfo/EP0712.java
@@ -0,0 +1,7 @@
+package clasesparaninfo;
+/*
+ * EP0712. Implementa una clase que permita resolver ecuaciones de segundo grado. Los coeficientes pueden indicarse en el constructor y modificarse a posteriori. Es fundamental que la clase disponga de un método que devuelva las distintas soluciones y de un método que nos informe si el discriminante es positivo.
+ */
+public class EP0712 {
+    
+}
---
+++ UD03/1. Clases/clasesparaninfo/EP0714.java
@@ -0,0 +1,7 @@
+package clasesparaninfo;
+/*
+ * EP0714. Crea una clase que sea capaz de mostrar el importe de un cambio, por ejemplo, al realizar una compra, con el menor número de monedas y billetes posibles. 
+ */
+public class EP0714 {
+    
+}
