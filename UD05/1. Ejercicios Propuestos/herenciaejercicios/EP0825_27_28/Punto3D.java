b875bbb7326a3d1fef6dd2111ea89e915ec87ff7
0scarDEV <146084578+0scarDEV@users.noreply.github.com>
2024-02-26 17:22:46 +0100

    error

--- UD05/1. Ejercicios Propuestos/herenciaejercicios/EP0825_27_28/Punto3D.java
+++ UD05/1. Ejercicios Propuestos/herenciaejercicios/EP0825_27_28/Punto3D.java
@@ -1,23 +1,25 @@
-package herenciaejercicios.EP0825_27_28;
-/* Óscar Fernández Pastoriza */
-public class Punto3D extends Punto {
-    int z;
-    public Punto3D(int x, int y, int z) {
-        super(x, y);
-        this.z = z;
-    }
-    double distancia(Punto3D otroPunto) {
-        return Math.sqrt(Math.pow((this.x - otroPunto.x), 2) + Math.pow((this.y - otroPunto.y),2) + Math.pow((this.z - otroPunto.z), 2));
-    }
-    /* EP0827. Implementa el método equals() para las clases Punto y Punto3D, teniendo en cuenta que dos puntos son iguales solo si tienen todas sus coordenadas iguales. */
-    @Override public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Punto3D punto3D = (Punto3D) o;
-        return x == punto3D.x && y == punto3D.y && z == punto3D.z;
-    }
-}
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE reservas SYSTEM "UD3_T6_tarefaA_ÓscarFernándezPastoriza.dtd">
+<reservas>
+  <reserva cliente="CLI1" tipo="AD">
+    <data_inicio ano="2013" mes="febreiro" día="5">
+      <observacións>Chegan sobre as 23:30</observacións>
+    </data_inicio>
+    <data_fin ano="2013" mes="febreiro" día="9"/>
+  </reserva>
+  <reserva cliente="CLI2" tipo="MP">
+    <data_inicio ano="2013" mes="febreiro" día="7"/>
+    <data_fin ano="2013" mes="febreiro" día="15">
+      <observacións>Saída as 16:00</observacións>
+    </data_fin>
+  </reserva>
+  <reserva cliente="CLI1" tipo="TI">
+    <data_inicio ano="2013" mes="febreiro" día="18"/>
+    <data_fin ano="2013" mes="febreiro" día="23"/>
+  </reserva>
+  <cliente id="CLI1">
+    <nome>Eva</nome>
+    <apelidos>Sobral García</apelidos>
+    <mobil>688998899</mobil>
+    <correo>eva.sobral@meucorreo.com</correo>
+  </clie
